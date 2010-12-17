df5be7c645cc98b45050f1415a596b7118636cfa
Nick Freear <nfreear@yahoo.co.uk>
2010-12-17 15:47:28 +0000

    Adding a 'en_utf8' language file, for Moodle 1.9.x compatibility.

---
+++ lang/en_utf8/filter_simplespeak.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Strings for component 'filter_simplespeak', language 'en'.
+ *
+ * @package   filter_simplespeak
+ * @copyright (c) 2010 Nicholas Freear {@link http://freear.org.uk}.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ *
+ * Moodle 1.9.x compatible.
+ */
+global $CFG;
+require_once $CFG->dirroot.'/filter/simplespeak/lang/en/filter_simplespeak.php';
