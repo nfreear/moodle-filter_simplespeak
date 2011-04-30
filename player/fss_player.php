2c1223db8495ff4bf654e8f7d4da821204a5f879
Nick Freear <nfreear@yahoo.co.uk>
2011-04-30 23:22:13 +0100

    CONTRIB-2884, Fix for Safari - HTML5 audio never autoplays. Plus, adding @cache parameter.

--- player/fss_player.php
+++ player/fss_player.php
@@ -13,10 +13,11 @@ global $CFG;
 require_login();
 
 $text    = required_param('q', PARAM_TEXT);
+$cache   = optional_param('cache', 1, PARAM_INT);
 $autoplay= (bool) optional_param('autoplay', 1, PARAM_INT);
 //$lang, $cache?
 
-$proxy_url = "$CFG->wwwroot/filter/simplespeak/proxy_tts/?q=".urlencode($text);
+$proxy_url = "$CFG->wwwroot/filter/simplespeak/proxy_tts/?q=$text&cache=$cache";
 
 // Prefer the lightweight, legacy MP3 player.
 $dir_mp3 = "$CFG->dirroot/lib/mp3player/mp3player.swf";
@@ -25,16 +26,16 @@ $autoplay_mp3 = $autoplay ? 'yes' : 'no';
 $dir_flow  = "CFG->dirroot/lib/flowplayer/flowplayer-3.2.7.swf";
 $flash_base= "$CFG->wwwroot/lib/flowplayer"; //flowplayer-3.2.7.swf;
 
-//HTML5 audio.
-$autoplay_html5 = $autoplay ?' autoplay ':'';
+//HTML5 audio - never autoplay (Safari).
+$autoplay_html5 = ''; //$autoplay ?' autoplay ':'';
 $html5_fallback =<<<EOF
-<audio controls $autoplay_html5>
+<audio controls$autoplay_html5>
  <source src="$proxy_url" type="audio/mpeg"/>
  [ Requires Flash or the HTML5 audio element. ]
 </audio>
 EOF;
 
-//Flowplayer: there's no explicit '.mp3' extension, so specify the audio plugin.
+//Flowplayer: there's no '.mp3' file extension, so specify the audio plugin.
 $flowplayer_config = array(
   'clip' => array(
     'url'=> $proxy_url,
@@ -47,7 +48,8 @@ $flowplayer_config = array(
     ),
   ),
 );
-$json = json_encode($flowplayer_config);
+$flowplayer_json = json_encode($flowplayer_config);
+
 ?>
 <!DOCTYPE html><html ><meta charset="utf-8"/><title><?php echo $text ?> [MP3 player]</title>
 <style>body{margin:0} object,audio{display:block; width:300px;height:40px;}</style>
@@ -60,7 +62,7 @@ $json = json_encode($flowplayer_config);
  <param name="quality" value="high"/>
  <param name="bgcolor" value="#555555"/>
  <param name="flashvars" value=
- "src=<?php echo $proxy_url; ?>&amp;autoPlay=<? echo $autoplay_mp3 ?>" />
+ "src=<?php echo urlencode($proxy_url); ?>&amp;autoPlay=<? echo $autoplay_mp3 ?>" />
 <?php echo $html5_fallback; ?>
 </object>
 <?php else: ?>
@@ -73,7 +75,7 @@ $json = json_encode($flowplayer_config);
  <param name="quality" value="high"/>
  <param name="bgcolor" value="#000000"/>
  <param name="flashvars" value=
- 'config=<?php echo $json; ?>' />
+ 'config=<?php echo $flowplayer_json; ?>' />
 <?php echo $html5_fallback; ?>
 </object>
 <?php endif; ?>
